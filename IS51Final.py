c3276b59fad5673e57c5aa43653f72986c8c0f42
Tyler Solley <tjsolley1@gmail.com>
2020-12-17 18:27:53 -0800

    pseudocode

--- IS51Final.py
+++ IS51Final.py
@@ -11,3 +11,14 @@ calculate percentage
 output results to user
 """
 
+"""
+define main fuction
+open and read Final.txt
+identify lenght and sum of grades
+divide sum by length
+print outputs to user
+create counter for each item in list above average
+calculate percentage by counter / lenght
+print ouput to user
+"""
+
