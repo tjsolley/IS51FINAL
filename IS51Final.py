648569db4d83fe79a12413d6c3d0f8562691393b
Tyler Solley <tjsolley1@gmail.com>
2020-12-17 18:24:07 -0800

    structured english

---
+++ IS51Final.py
@@ -0,0 +1,13 @@
+"""
+This program will be able to calculate the length, average, and percent
+above average for a list o grades.
+We will first define a main function
+open and read the text file
+outut the list of grade
+output the sum
+calculate average
+create a counter for each grade above average
+calculate percentage
+output results to user
+"""
+
